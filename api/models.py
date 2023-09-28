89ef7d822c8c76b7de74ac08ccae8e47f8fe5bdb
Andrey Orlov <Druni.Adler@yandex.ru>
2023-09-29 00:59:26 +0300

    добавил НОРМАЛЬНУЮ авторизацию по JWT, а не то костыльно-бабуинское нечто

--- api/models.py
+++ api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class Storage(models.Model):
+    name = models.CharField(max_length=255)
+    url = models.URLField(max_length=255)
+    access_key_id = models.CharField(max_length=255)
+    secret_access_key = models.CharField(max_length=255)
+    region = models.CharField(max_length=255)
+    bucket = models.CharField(max_length=255)
+
