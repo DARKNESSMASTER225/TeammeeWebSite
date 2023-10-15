8a7f3d42aea4d7a6ba5c5607575547732c7835ca
Andrey Orlov <Druni.Adler@yandex.ru>
2023-10-15 20:49:21 +0300

    И да ебутся мишки, мамки и все живые существа!
    Ъуъ сука

--- api/views.py
+++ api/views.py
@@ -1,5 +1,8 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
+from requests import Response
 from rest_framework import generics, permissions
+from rest_framework.decorators import api_view
 
 from api.models import Storage
 from api.serializers import StorageSerializer
@@ -12,6 +15,19 @@ class StorageListView(generics.ListAPIView):
     permission_classes = (permissions.IsAuthenticated,)
 
 
+@api_view(['POST'])
+def create_slave_profile(request):
+    email = request.POST.get['email']
+    password = request.POST.get['password']
+    profile = request.user.profile
+    profile.image = None
+    user = User(email=email, password=password, profile=profile).save()
+    return Response(user)
 
 
-
+@api_view(['GET'])
+def get_all_slaves_profile(request):
+    user = request.user
+    group_id = user.profile.group_id
+    slaves = User.objects.filter(profile__group=group_id)
+    return slaves
