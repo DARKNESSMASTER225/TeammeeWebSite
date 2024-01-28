5db990511b857dca5d3422f9805a8eceb784f886
DruniAdler <druni.adler@yandex.ru>
2024-01-29 01:06:03 +0300

    скачать

--- api/views.py
+++ api/views.py
@@ -135,8 +135,10 @@ def delete_user(request):
         try:
             user.delete()
         except AttributeError as e:
-            return Response(data={'error': str(e), 'snippet': f'Maybe DB don\'t have user with username "{username}"'},
-                            status=400)
+            return Response(data={"error": "Not Found",
+                                  'details': str(e),
+                                  'snippet': f'Maybe DB don\'t have user with username "{username}"'},
+                            status=404)
         return Response(data=user.id, status=201)
     else:
         return Response(status=401)
